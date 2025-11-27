da2179ccbaefce71c17f6b1b9fe159bd8069c40a
kodraa <mohammad_nour_10@hotmail.com>
2025-11-27 20:05:25 +0200

    Add Form Component

--- apps/web/src/lib/rhf-devtools/index.ts
+++ apps/web/src/lib/rhf-devtools/index.ts
@@ -1,5 +1,4 @@
-export { default as RHFDevtoolsPanel } from './DevtoolsPanel'
-export { useRHFDevtools } from './useRHFDevtools'
-export { DevtoolsEventClient } from './eventClient'
-export type { FormState } from './eventClient'
-
+export { default as RHFDevtoolsPanel } from "./DevtoolsPanel";
+export { useRHFDevtools } from "./useRHFDevtools";
+export { DevtoolsEventClient } from "./eventClient";
+export type { FormState } from "./eventClient";
